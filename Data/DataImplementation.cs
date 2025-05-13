9cbe0094dafc3c5c1b434fcc2801ca7a361538f8
eszymans <edytakociak@gmail.com>
2025-05-13 12:53:08 +0200

    every ball has their thread

--- Data/DataImplementation.cs
+++ Data/DataImplementation.cs
@@ -1,18 +1,9 @@
-﻿//____________________________________________________________________________________________________________________________________
-//
-//  Copyright (C) 2024, Mariusz Postol LODZ POLAND.
-//
-//  To be in touch join the community by pressing the `Watch` button and get started commenting using the discussion panel at
-//
-//  https://github.com/mpostol/TP/discussions/182
-//
-//_____________________________________________________________________________________________________________________________________
-
-using System;
+﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace TP.ConcurrentProgramming.Data
 {
@@ -22,14 +13,15 @@ namespace TP.ConcurrentProgramming.Data
 
         public DataImplementation()
         {
-            MoveTimer = new Timer(Move, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(15));
+            CollisionThread = new Thread(CollisionLoop);
+            CollisionThread.Start();
         }
 
         #endregion ctor
 
         #region DataAbstractAPI
 
-        public override async Task Start(int numberOfBalls, Action<IVector, Double, IBall> upperLayerHandler)
+        public override async Task Start(int numberOfBalls, Action<IVector, double, IBall> upperLayerHandler)
         {
             if (Disposed)
                 throw new ObjectDisposedException(nameof(DataImplementation));
@@ -42,12 +34,16 @@ namespace TP.ConcurrentProgramming.Data
                 for (int i = 0; i < numberOfBalls; i++)
                 {
                     Vector startingPosition = new(random.Next(100, 300), random.Next(100, 300));
-                    Vector initialVelocity = new((random.NextDouble() - 0.5) * 75, (random.NextDouble() - 0.5) * 75);
+                    Vector initialVelocity = new((random.NextDouble() - 0.5) * 150, (random.NextDouble() - 0.5) * 150);
                     double mass = random.NextDouble() * 10 + 10;
                     double radius = random.NextDouble() * 10 + 10;
                     Ball newBall = new(startingPosition, initialVelocity, radius, mass);
                     upperLayerHandler(startingPosition, radius, newBall);
                     BallsList.Add(newBall);
+
+                    Thread ballThread = new Thread(() => BallThreadLoop(newBall));
+                    BallThreads[newBall] = ballThread;
+                    ballThread.Start();
                 }
             });
         }
@@ -62,13 +58,27 @@ namespace TP.ConcurrentProgramming.Data
             {
                 if (disposing)
                 {
-                    MoveTimer.Dispose();
+                    stopThreads = true;
+
+                    foreach (var thread in BallThreads.Values)
+                    {
+                        if (thread.IsAlive)
+                            thread.Join();
+                    }
+
+                    if (CollisionThread.IsAlive)
+                        CollisionThread.Join();
+
+                    BallThreads.Clear();
                     BallsList.Clear();
                 }
+
                 Disposed = true;
             }
             else
+            {
                 throw new ObjectDisposedException(nameof(DataImplementation));
+            }
         }
 
         public override void Dispose()
@@ -79,15 +89,35 @@ namespace TP.ConcurrentProgramming.Data
 
         #endregion IDisposable
 
-        #region private
+        #region Private fields
 
         private bool Disposed = false;
-        private DateTime lastUpdateTime = DateTime.Now;
+        private volatile bool stopThreads = false;
 
-        private readonly Timer MoveTimer;
-        private Random RandomGenerator = new();
         private ConcurrentBag<Ball> BallsList = new();
-        private readonly object lockObject = new();
+        private ConcurrentDictionary<Ball, Thread> BallThreads = new();
+        private readonly object ballsLock = new();
+        private readonly Thread CollisionThread;
+
+        #endregion
+
+        #region Movement
+
+        private void BallThreadLoop(Ball ball)
+        {
+            DateTime lastUpdate = DateTime.Now;
+
+            while (!stopThreads)
+            {
+                DateTime now = DateTime.Now;
+                double deltaTime = (now - lastUpdate).TotalSeconds;
+                lastUpdate = now;
+
+                MoveBall(ball, deltaTime);
+
+                Thread.Sleep(10); // ok. 60 FPS
+            }
+        }
 
         private void MoveBall(Ball ball, double deltaTime)
         {
@@ -96,84 +126,99 @@ namespace TP.ConcurrentProgramming.Data
                 ball.Velocity.y * deltaTime
             );
 
-            ball.Move(scaledDelta);
+                ball.Move(scaledDelta);
         }
 
-        private void Move(object? state)
+        #endregion
+
+        #region Collision handling
+
+        private void CollisionLoop()
         {
-            lock (lockObject)
+            while (!stopThreads)
             {
-                DateTime now = DateTime.Now;
-                double deltaTime = (now - lastUpdateTime).TotalSeconds;
-                lastUpdateTime = now;
-
-                var moveTasks = BallsList.Select(ball => Task.Run(() => MoveBall(ball, deltaTime))).ToArray();
-
-                Task.WhenAll(moveTasks).Wait();
-
                 HandleBallCollisions();
+                Thread.Sleep(15);
             }
         }
 
         private void HandleBallCollisions()
         {
-            var balls = BallsList.ToArray();
+            Ball[] balls = BallsList.ToArray();
 
-            Parallel.For(0, balls.Length, i =>
+            for (int i = 0; i < balls.Length; i++)
             {
                 Ball b1 = balls[i];
                 for (int j = i + 1; j < balls.Length; j++)
                 {
                     Ball b2 = balls[j];
 
-                    Vector pos1 = b1.Position;
-                    Vector pos2 = b2.Position;
+                    // Aby uniknąć deadlocka — zawsze blokuj w tej samej kolejności
+                    Ball first = b1.GetHashCode() < b2.GetHashCode() ? b1 : b2;
+                    Ball second = b1.GetHashCode() < b2.GetHashCode() ? b2 : b1;
 
-                    double dx = pos2.x - pos1.x;
-                    double dy = pos2.y - pos1.y;
-                    double distance = Math.Sqrt(dx * dx + dy * dy);
+                    lock (first)
+                        lock (second)
+                        {
+                            Vector pos1 = b1.Position;
+                            Vector pos2 = b2.Position;
 
-                    double minDistance = b1.Radius + b2.Radius;
+                            double dx = pos2.x - pos1.x;
+                            double dy = pos2.y - pos1.y;
+                            double distance = Math.Sqrt(dx * dx + dy * dy);
+                            double minDistance = b1.Radius + b2.Radius;
 
-                    if (distance < minDistance && distance > 0)
-                    {
-                        double nx = dx / distance;
-                        double ny = dy / distance;
+                            if (distance < minDistance && distance > 0)
+                            {
+                                // Normalna kolizji
+                                double nx = dx / distance;
+                                double ny = dy / distance;
 
-                        IVector v1 = b1.Velocity;
-                        IVector v2 = b2.Velocity;
+                                // Separacja pozycji
+                                double overlap = minDistance - distance + 0.01;
+                                double totalMass = b1.Mass + b2.Mass;
 
-                        double p = 2 * (v1.x * nx + v1.y * ny - v2.x * nx - v2.y * ny) / (b1.Mass + b2.Mass);
+                                double correctionB1 = (b2.Mass / totalMass) * overlap;
+                                double correctionB2 = (b1.Mass / totalMass) * overlap;
 
-                        Vector newV1 = new Vector(
-                            v1.x - p * b2.Mass * nx,
-                            v1.y - p * b2.Mass * ny
-                        );
+                                b1.Position = new Vector(pos1.x - nx * correctionB1, pos1.y - ny * correctionB1);
+                                b2.Position = new Vector(pos2.x + nx * correctionB2, pos2.y + ny * correctionB2);
 
-                        Vector newV2 = new Vector(
-                            v2.x + p * b1.Mass * nx,
-                            v2.y + p * b1.Mass * ny
-                        );
+                                // Prędkości przed kolizją
+                                IVector v1 = b1.Velocity;
+                                IVector v2 = b2.Velocity;
 
-                        lock (b1) b1.Velocity = newV1;
-                        lock (b2) b2.Velocity = newV2;
+                                // Składowe wzdłuż wektora normalnego
+                                double v1n = v1.x * nx + v1.y * ny;
+                                double v2n = v2.x * nx + v2.y * ny;
 
-                        double overlap = minDistance - distance;
-                        double totalMass = b1.Mass + b2.Mass;
+                                // Jeśli już się oddalają, pomiń
+                                if (v1n - v2n <= 0)
+                                    continue;
 
-                        double correctionB1Factor = b2.Mass / totalMass;
-                        double correctionB2Factor = b1.Mass / totalMass;
+                                // Całkowicie sprężyste zderzenie: nowa składowa wzdłuż normalnej
+                                double m1 = b1.Mass;
+                                double m2 = b2.Mass;
 
-                        Vector correction = new Vector(nx * overlap, ny * overlap);
+                                double newV1n = (v1n * (m1 - m2) + 2 * m2 * v2n) / (m1 + m2);
+                                double newV2n = (v2n * (m2 - m1) + 2 * m1 * v1n) / (m1 + m2);
 
-                        lock (b1) b1.Position = new Vector(pos1.x - correction.x * correctionB1Factor, pos1.y - correction.y * correctionB1Factor);
-                        lock (b2) b2.Position = new Vector(pos2.x + correction.x * correctionB2Factor, pos2.y + correction.y * correctionB2Factor);
-                    }
+                                // Składowe styczne pozostają bez zmian
+                                double v1t_x = v1.x - v1n * nx;
+                                double v1t_y = v1.y - v1n * ny;
+                                double v2t_x = v2.x - v2n * nx;
+                                double v2t_y = v2.y - v2n * ny;
+
+                                // Nowe prędkości
+                                b1.Velocity = new Vector(v1t_x + newV1n * nx, v1t_y + newV1n * ny);
+                                b2.Velocity = new Vector(v2t_x + newV2n * nx, v2t_y + newV2n * ny);
+                            }
+                        }
                 }
-            });
+            }
         }
 
-        #endregion private
+        #endregion
 
         #region TestingInfrastructure
 
@@ -195,6 +240,6 @@ namespace TP.ConcurrentProgramming.Data
             returnInstanceDisposed(Disposed);
         }
 
-        #endregion TestingInfrastructure
+        #endregion
     }
 }
